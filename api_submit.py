f687880f2d3fbc4ac17037ffb1327f68522e0ad7
Starcutter <zuohj15@mails.tsinghua.edu.cn>
2018-05-01 03:41:36 +0800

    feat: finish day 1 submission

--- api_submit.py
+++ api_submit.py
@@ -3,13 +3,13 @@
 
 import requests
 
-files={'files': open('sample_submission.csv','rb')}
+files={'files': open('submit.csv','rb')}
 
 data = {
-    "user_id": "",   #user_id is your username which can be found on the top-right corner on our website when you logged in.
-    "team_token": "", #your team_token.
-    "description": 'your description',  #no more than 40 chars.
-    "filename": "file_name", #your filename
+    "user_id": "meta_z",   #user_id is your username which can be found on the top-right corner on our website when you logged in.
+    "team_token": "65355b59c393aec8e71eaa3b1cdb70a5692e8a61816a37cde79a2bed49bba644", #your team_token.
+    "description": 'submission 2018-05-01',  #no more than 40 chars.
+    "filename": "submit.csv", #your filename
 }
 
 url = 'https://biendata.com/competition/kdd_2018_submit/'
