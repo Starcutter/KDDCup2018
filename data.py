ae49c3a8efd2ecfcd058b83751552b05d2000f40
Wind <xrz199721@126.com>
2018-04-30 23:08:20 +0800

    feat: able to fetch meo datas from file and api

--- data.py
+++ data.py
@@ -27,7 +27,7 @@ beijing_aq = beijing_aq.rename(columns={"stationId": "station_id"})
 
 # # London AQ
 
-# In[4]:
+# In[48]:
 
 
 df1 = pd.read_csv('data/London_historical_aqi_forecast_stations_20180331.csv', index_col=0)
@@ -41,9 +41,47 @@ london_aq = london_aq.rename(columns={"MeasurementDateGMT": "utc_time"})
 london_aq["utc_time"] = pd.to_datetime(london_aq["utc_time"])
 
 
+# # Beijing grid meteorology
+
+# In[68]:
+
+
+df = pd.read_csv("data/Beijing_historical_meo_grid.csv")
+
+
+# In[71]:
+
+
+beijing_meo = df.rename(columns={
+    "stationName": "station_id",
+    "utc_time": "time",
+    "wind_speed/kph": "wind_speed",
+})
+beijing_meo["time"] = pd.to_datetime(beijing_meo["time"])
+
+
+# # London grid meteorology
+
+# In[63]:
+
+
+df = pd.read_csv("data/London_historical_meo_grid.csv")
+
+
+# In[66]:
+
+
+london_meo = df.rename(columns={
+    "stationName": "station_id",
+    "utc_time": "time",
+    "wind_speed/kph": "wind_speed"
+})
+london_meo["time"] = pd.to_datetime(london_meo["time"])
+
+
 # # APIs
 
-# In[25]:
+# In[103]:
 
 
 import requests
@@ -65,20 +103,25 @@ def buildDataFrame(text):
 
 def airQualityData(city="bj", year=2018, month=5, day=1, hour=0):
     if city == "bj":
-        target = ["station_id", "PM2.5", "PM10", "O3"]
+        target = ["station_id", "time", "PM2.5", "PM10", "O3"]
     else:
-        target = ["station_id", "PM2.5", "PM10"]
+        target = ["station_id", "time", "PM2.5", "PM10"]
     dic = {
         "PM25_Concentration": "PM2.5",
         "PM10_Concentration": "PM10",
         "O3_Concentration": "O3",
         "PM10 (ug/m3)": "PM10",
         "PM2.5 (ug/m3)": "PM2.5",
+        "utc_time": "time",
     }
     if year == 2018 and month >= 4:
         url = 'https://biendata.com/competition/airquality/%s/%d-%d-%d-%d/%d-%d-%d-%d/2k0d1d8' % (city, year, month, day, hour, year, month, day, hour)
         response = requests.get(url)
         df = buildDataFrame(response.text)
+        try:
+            df["time"] = pd.to_datetime(df["time"])
+        except:
+            pass
     else:
         time = pd.Timestamp("%d-%d-%d %d:00:00" % (year, month, day, hour))
         if city == "bj":
@@ -92,13 +135,36 @@ def airQualityData(city="bj", year=2018, month=5, day=1, hour=0):
         df = df[target]
     except:
         pass
+    if df.empty:
+        df = pd.DataFrame()
     return df
 
 
 def meteorologyGridData(city="bj", year=2018, month=5, day=1, hour=0):
+    target = ["station_id", "time", "temperature", "pressure",
+              "humidity", "wind_direction", "wind_speed"]
     url = 'https://biendata.com/competition/meteorology/%s_grid/%d-%d-%d-%d/%d-%d-%d-%d/2k0d1d8' % (city, year, month, day, hour, year, month, day, hour)
-    response = requests.get(url)
-    return buildDataFrame(response.text)
+    if year == 2018 and month >= 4:
+        response = requests.get(url)
+        df = buildDataFrame(response.text)
+        try:
+            df["time"] = pd.to_datetime(df["time"])
+        except:
+            pass
+    else:
+        time = pd.Timestamp("%d-%d-%d %d:00:00" % (year, month, day, hour))
+        if city == "bj":
+            df = beijing_meo[beijing_meo["time"] == time]
+        else:
+            df = london_meo[london_meo["time"] == time]
+    try:
+        df = df[target]
+    except:
+        pass
+    df = df.reset_index(drop=True)
+    if df.empty:
+        df = pd.DataFrame()
+    return df
 
 
 def observedMeteorology(city="bj", year=2018, month=5, day=1, hour=0):
@@ -107,5 +173,11 @@ def observedMeteorology(city="bj", year=2018, month=5, day=1, hour=0):
         return
     url = 'https://biendata.com/competition/meteorology/%s/%d-%d-%d-%d/%d-%d-%d-%d/2k0d1d8' % (city, year, month, day, hour, year, month, day, hour)
     response = requests.get(url)
-    return buildDataFrame(response.text)
+    df = buildDataFrame(response.text)
+    if df.empty:
+        df = pd.DataFrame()
+    return df
+
+# airQualityData("ld", 2017, 4, 26, 2)
+meteorologyGridData("ld", 2018, 4, 26, 2)
 
